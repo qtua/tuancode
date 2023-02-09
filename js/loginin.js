45f84e80a1927812fa251fe60102c8537d64d763
nguyenquangtuann <119723610+nguyenquangtuann@users.noreply.github.com>
2023-02-09 15:34:22 +0700

    s

---
+++ js/loginin.js
@@ -0,0 +1,13 @@
+function validate() {
+    var username = document.getElementById("name").value;
+    var password = document.getElementById("pass").value;
+    if (username == "admin@gmail.com" && password == "123456") {
+        // alert("Đăng nhập thành công");
+        window.location = "main.html";
+    }
+    else {
+        alert("Đăng nhập không thành công");
+        document.getElementById("pass").value = "";
+    }
+    
+}
